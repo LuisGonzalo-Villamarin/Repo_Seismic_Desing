6bc199067bfdc2064b3c91524ba049d37625f8e9
Carlos Celi <62081230+Normando1945@users.noreply.github.com>
2026-05-22 11:44:12 -0500

    update repo

--- repo_seismic_desing/__init__.py
+++ repo_seismic_desing/__init__.py
@@ -2,7 +2,8 @@ from .core_simple_class_DS import (Simple_free_motion, plt_amp_tim,
                                    Simple_free_motion_sub_damping,
                                    Step_by_Step_BNewmark,
                                    SPEC_BNewmark,
-                                   SPEC_NEC_2024,)
+                                   SPEC_NEC_2024,
+                                   Eig_Normalize)
 
 __all__ = [
     "Simple_free_motion",
@@ -10,5 +11,6 @@ __all__ = [
     "Simple_free_motion_sub_damping",
     "Step_by_Step_BNewmark",
     "SPEC_BNewmark",
-    "SPEC_NEC_2024"
+    "SPEC_NEC_2024",
+    "Eig_Normalize"
 ]
